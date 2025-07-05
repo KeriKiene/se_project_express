4b8b0d4e31b85221ceb26f038c28911a35dfdf8a
Keri Kiene <mylifeismychild@yahoo.com>
2025-07-05 16:42:32 -0600

    Updates

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3001, () => {
-  console.log("Listening on port 3001");
+const { PORT = 3001 } = process.env;
+
+app.listen(PORT, () => {
+  console.log("Listening on port ${PORT}");
 });
